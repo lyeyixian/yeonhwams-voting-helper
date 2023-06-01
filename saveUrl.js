2bf8ac312cc351a23c5c55259b0ee7baf902a656
Lye Yi Xian <lyeyx999@hotmail.com>
2023-06-01 18:02:05 +0800

    Remove console log

--- saveUrl.js
+++ saveUrl.js
@@ -1,9 +1,9 @@
 function saveUrl() {
   const url = window.location.toString()
-  console.log(url)
+
   chrome.storage.local.get('urls', (res) => {
     const newUrls = res.urls || []
-    console.log(newUrls)
+
     newUrls.push(url)
     chrome.storage.local.set({ urls: newUrls }, () => console.log('saved'))
   })
